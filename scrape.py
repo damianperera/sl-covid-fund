4ba52033b7328619f79c8bb53c791fa39991125a
Damian Perera <15967502+damianperera@users.noreply.github.com>
2021-05-09 20:58:58 +0200

    enhancement: added donations obj to json (#6)

--- scrape.py
+++ scrape.py
@@ -3,33 +3,62 @@ from lxml import html
 import json
 from datetime import datetime
 
-source = 'https://www.itukama.lk/'
-
 # Execution time
 time = datetime.now().isoformat()
 
-# Get fund value
-pageContent = requests.get(source)
-tree = html.fromstring(pageContent.content)
-fundVal = tree.xpath('string(/html/body/div[1]/div[2]/section[1]/div/div[2]/div[1]/div/div[1]/span/i/text())')
-print('Fund value was LKR ', fundVal)
+fundSource = 'https://www.itukama.lk/'
+fundContent = requests.get(fundSource)
+fundTree = html.fromstring(fundContent.content)
+
+donationsSource = 'https://www.itukama.lk/donate-now/'
+donationsContent = requests.get(donationsSource)
+donationsTree = html.fromstring(donationsContent.content)
+
+def getLastDonatedAt():
+	arr = donationsTree.xpath('string(//*[@id="donated-list"]/tbody/tr[1]/td[1]/text())').split(' ')
+	return ' '.join([arr[-3], arr[-2], arr[-1]])
+
+def getLastDonatedBy():
+	return donationsTree.xpath('string(//*[@id="donated-list"]/tbody/tr[1]/td[1]/text())').split()[0]
+
+def getDonationCount():
+	return donationsTree.xpath('string(/html/body/div[1]/div[2]/section[2]/div/div/div[2]/div[4]/div/h3/span/text())')
+
+def getLastDonatedAmount():
+	return donationsTree.xpath('string(//*[@id="donated-list"]/tbody/tr[1]/td[2]/text())')
+
+def getFundValue():
+	return fundTree.xpath('string(/html/body/div[1]/div[2]/section[1]/div/div[2]/div[1]/div/div[1]/span/i/text())')
+
+# Build latest fund data
+fundData = {}
+fundData['value'] = getFundValue()
+fundData['source'] = fundSource
+fundData['time'] = time
+print('Fund: ', fundData)
+
+# Build latest donation data
+donationData = {}
+donationData['count'] = getDonationCount()
+donationData['lastDonatedAmount'] = getLastDonatedAmount()
+donationData['lastDonatedAt'] = getLastDonatedAt()
+donationData['lastDonatedBy'] = getLastDonatedBy()
+donationData['lastUpdated'] = time
+print('Donatins: ', donationData)
 
 with open('data.json', 'r+') as persistentFile:
 	# Load historical data
 	historicalData = json.load(persistentFile)
 	historicalData['lastUpdated'] = time
-	
-	# Build latest data
-	data = {}
-	data['value'] = fundVal
-	data['source'] = source
-	data['time'] = time
+
+	if not 'donations' in historicalData or historicalData['donations']['count'] != donationData['count']:
+		historicalData['donations'] = donationData
 
 	if not 'history' in historicalData:
 		historicalData['history'] = []
-		historicalData['history'].append(data)
-	elif historicalData['history'][-1]['value'] != fundVal:
-		historicalData['history'].append(data)
+		historicalData['history'].append(fundData)
+	elif historicalData['history'][-1]['value'] != fundData['value']:
+		historicalData['history'].append(fundData)
 
 	# Save
 	persistentFile.seek(0)
